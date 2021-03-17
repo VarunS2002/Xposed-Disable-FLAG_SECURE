b528d458b32d567050300b238af50dc21c1c763c
VarunS2002 <30069380+VarunS2002@users.noreply.github.com>
2021-03-17 12:36:18 +0530

    Added Main Code

--- app/src/main/java/com/varuns2002/disable_flag_secure/DisableFlagSecure.kt
+++ app/src/main/java/com/varuns2002/disable_flag_secure/DisableFlagSecure.kt
@@ -1,5 +1,53 @@
 package com.varuns2002.disable_flag_secure
 
+import android.view.SurfaceView
+import android.view.Window
+import android.view.WindowManager
+import de.robv.android.xposed.IXposedHookLoadPackage
+import de.robv.android.xposed.XC_MethodHook
+import de.robv.android.xposed.XposedBridge
+import de.robv.android.xposed.XposedHelpers
+import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam
 
-class DisableFlagSecure {
+class DisableFlagSecure : IXposedHookLoadPackage {
+
+    private val mRemoveSecureFlagHook: XC_MethodHook = object : XC_MethodHook() {
+        override fun beforeHookedMethod(param: MethodHookParam) {
+            var flags: Int = param.args[0] as Int
+            flags = flags and WindowManager.LayoutParams.FLAG_SECURE.inv()
+            param.args[0] = flags
+        }
+    }
+
+    private val mRemoveSetSecureHook: XC_MethodHook = object : XC_MethodHook() {
+        override fun beforeHookedMethod(param: MethodHookParam) {
+            param.args[0] = false
+        }
+    }
+
+    /**
+     * This method is called when an app is loaded. It's called very early, even before
+     * [Application.onCreate] is called.
+     * Modules can set up their app-specific hooks here.
+     *
+     * @param lpparam Information about the app.
+     * @throws Throwable Everything the callback throws is caught and logged.
+     */
+    @Suppress("KDocUnresolvedReference")
+    override fun handleLoadPackage(lpparam: LoadPackageParam?) {
+        if (lpparam != null) {
+            // Log Package Name
+            XposedBridge.log("Disable-Flag_Secure Loaded app: " + lpparam.packageName)
+        }
+
+        XposedHelpers.findAndHookMethod(
+            Window::class.java, "setFlags", Int::class.javaPrimitiveType, Int::class.javaPrimitiveType,
+            mRemoveSecureFlagHook
+        )
+
+        XposedHelpers.findAndHookMethod(
+            SurfaceView::class.java, "setSecure", Boolean::class.javaPrimitiveType,
+            mRemoveSetSecureHook
+        )
+    }
 }
